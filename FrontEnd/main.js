cd250043a72f53978f3655049996550e86b43acf
Mohamed Abdalla <78317393+Mohman556@users.noreply.github.com>
2022-12-03 17:44:48 -0500

    Landing page

---
+++ FrontEnd/main.js
@@ -0,0 +1,40 @@
+
+
+
+/* ========================================================================
+ * Header PopUps
+ * ======================================================================== */
+
+//Refrences Popup
+const refPop = document.querySelector(".refPop");
+const ref_closeButton = document.querySelector(".ref-close-button");
+
+function refrenceFunction(){
+  refPop.classList.toggle("show-refpopup");
+}
+
+function refwindowOnClick(event){
+  if (event.target === refPop){
+    refrenceFunction();
+  }
+}
+
+ref_closeButton.addEventListener("click",refrenceFunction);
+window.addEventListener("click", refwindowOnClick);
+
+//Contacts Popup
+const conPop = document.querySelector(".conPop");
+const con_closeButton = document.querySelector(".con-close-button");
+
+function contactFunction(){
+  conPop.classList.toggle("show-conpopup");
+}
+
+function conwindowOnClick(event){
+  if (event.target === conPop){
+    contactFunction();
+  }
+}
+
+con_closeButton.addEventListener("click",contactFunction);
+window.addEventListener("click", conwindowOnClick);
